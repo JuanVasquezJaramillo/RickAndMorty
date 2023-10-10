dcc6a97e1b8f2d44e35f016c624297bc6601bca0
JuanVasquezJaramillo <juapablovasquez2003@gmail.com>
2023-10-10 16:59:56 -0500

    Cambios en detail 10/10/23 5.00pm

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -5,8 +5,7 @@ import { useState } from 'react';
 import estilo from './detail.module.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { ObtenerEpisodios, ObtenerPorId } from '../../redux/actions';
-
-
+import NavBar from '../NavBar/NavBar';
 const Detail = () => {
     const dispatch = useDispatch();
     const { id } = useParams();
@@ -31,35 +30,58 @@ const Detail = () => {
 
 
     return (
-        <div className={estilo.contenedor}>
+        <div>
             <div>
-                <img src={character?.image && character.image} alt='' className={estilo.pruebaImg} />
-                <h1>Name: {character?.name && character.name}</h1>
-                <h1>Status: {character?.status && character.status}</h1>
-                <h1>Species: {character?.species && character.species}</h1>
-                <h1>Gender: {character?.gender && character.gender}</h1>
-                <h1>Origin: {character?.origin?.name && character.origin?.name}</h1>
+                <NavBar></NavBar>
             </div>
-            <div>
-                {
-                    character.episode && (
-                        <>
-                            <h2>
-                                Primera aparición:
-                                {character.episode[0]}
-                            </h2>
-                            <h2>
-                                Última aparición:
-                                {character.episode[character.episode.length > 1 ? character.episode.length - 1 : 0]}
-                            </h2>
-                        </>
-                    )
-                }
+            <div className={estilo.contenedorPadre}>
+                <div className={estilo.contenedorInfo}>
+                    <img src={character?.image && character.image} alt='' className={estilo.pruebaImg} />
+                    <h1>Name: {character?.name && character.name}</h1>
+                    <h1>Status: {character?.status && character.status}</h1>
+                    <h1>Species: {character?.species && character.species}</h1>
+                    <h1>Gender: {character?.gender && character.gender}</h1>
+                    <h1>Origin: {character?.origin?.name && character.origin?.name}</h1>
+                </div>
+                <div className={estilo.contenedorEpisode}>
+                    <div className={estilo.subContenedorEpisode}>
+
+                        {
+                            character.episode && (
+                                <div>
+                                    <>
+                                        <h2 className={estilo.titleP1}>Primera aparición:</h2>
+                                        <p className={estilo.parrafo}>
+                                            {character.episode[0]}
+                                        </p>
+                                        <div className={estilo.contenedorVideo}>
+                                            <iframe width="703" height="350" src="https://www.youtube.com/embed/TLzuSk13Wsw" title="Rick and Morty | Pain is Pleasure | Adult Swim UK 🇬🇧" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                        </div>
+                                    </>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className={estilo.subContenedorEpisode2}>
+                        {
+                            character.episode && (
+                                <div>
+                                    <>
+                                        <h3 className={estilo.titleP1}>
+                                            Última aparición:
+                                        </h3>
+                                        <p className={estilo.parrafo}>
+                                            {character.episode[character.episode.length > 1 ? character.episode.length - 1 : 0]}
+                                        </p>
+                                        <iframe width="703" height="403" src="https://www.youtube.com/embed/TLzuSk13Wsw" title="Rick and Morty | Pain is Pleasure | Adult Swim UK 🇬🇧" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                    </>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
             </div>
-            <Link to='/home'>
-                <button className={estilo.boton}>inicio</button>
-            </Link>
-        </div>
+        </div >
     )
 }
 export default Detail;
