9f0ef0b413c5c5ca6fbee6cb201db19e3d611d2a
JuanVasquezJaramillo <juapablovasquez2003@gmail.com>
2024-04-30 12:41:10 -0500

    New Parallax Efect added

--- client/src/views/LandingPage/Landing.jsx
+++ client/src/views/LandingPage/Landing.jsx
@@ -1,15 +1,23 @@
 import NavBar from '../../components/NavBar/NavBar';
 import style from './landing.module.css';
 import Header from '../../components/Header/Header';
+import ManualParallax from '../../components/ManualParallax/ManualParallax';
 
 const Landing = () => {
     return (
         <>
             <div>
-                <Header></Header>
+                <div>
+                    <Header></Header>
+                </div>
+                <div className={style.container}>
+                    <NavBar />
+                </div>
             </div>
-            <div className={style.container}>
-                <NavBar />
+            <div>
+                <div>
+                    <ManualParallax />
+                </div>
             </div>
         </>
     )
